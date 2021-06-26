f14a7bab6bdfbe1c03f31d88d9f85fad027f116e
Hanan Neor <hananneor@gmail.com>
2021-06-27 02:27:27 +0300

    -

--- js/cmps/app-header.js
+++ js/cmps/app-header.js
@@ -6,8 +6,9 @@ export default {
         <h3>Miss Books</h3>
     </div>
     <nav>
-        <router-link to="/">Home</router-link> |
-        <router-link to="/book">Books</router-link> |
+        <!-- <router-link to="/">Home</router-link> | -->
+        <!-- <router-link to="/book">Books</router-link> | -->
+
         <!-- <router-link to="/about">About</router-link> | -->
         <a href="https://hanan-neor.github.io/Appsus/#/">Back to Appsus</a>
     </nav>
